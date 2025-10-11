91a60c189588455df480f6f4e1e4ea5b657d8cdb
Cheng-I-Lin <alan.lin.1118@gmail.com>
2025-10-11 15:47:16 -0700

    Use js to code nav links

--- global.js
+++ global.js
@@ -4,10 +4,34 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+// let navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+  
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/Cheng-I-Lin', title: 'Github' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    // Create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
