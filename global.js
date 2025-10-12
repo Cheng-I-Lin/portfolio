de89a7a1fa05031a89e301c211c0aaa50e6a91dc
Cheng-I-Lin <alan.lin.1118@gmail.com>
2025-10-11 19:12:04 -0700

    Styled current page nav

--- global.js
+++ global.js
@@ -32,6 +32,17 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
     url = !url.startsWith('http') ? BASE_PATH + url : url;
+    
     // Create link and add it to nav
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    //nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    } else if (a.host != location.host) {
+        a.target = "_blank";
+    }
 }
