85227784827c48854ecc9f292a554743862a05dc
Cheng-I-Lin <alan.lin.1118@gmail.com>
2025-10-26 21:18:20 -0700

    Used d3 to graph project year distribution

--- projects/projects.js
+++ projects/projects.js
@@ -5,15 +5,17 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let data = [
-    { value: 1, label: 'apples' },
-    { value: 2, label: 'oranges' },
-    { value: 3, label: 'mangos' },
-    { value: 4, label: 'pears' },
-    { value: 5, label: 'limes' },
-    { value: 5, label: 'cherries' },
-];
+let data = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+});
+
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
