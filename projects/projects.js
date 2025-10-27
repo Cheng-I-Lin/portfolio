2ba7cb94371406fa0ea790f4f33d40131c56fc62
Cheng-I-Lin <alan.lin.1118@gmail.com>
2025-10-26 22:21:29 -0700

    Make reactive graph and search bar

--- projects/projects.js
+++ projects/projects.js
@@ -36,3 +36,61 @@ data.forEach((d, idx) => {
     .attr('class', 'legend-items')
     .html(`<span class="swatch"></span> ${d.label} <em class="legend-value">(${d.value})</em>`); // set the inner html of <li>
 });
+
+let searchInput = document.querySelector('.searchBar');
+
+function setQuery(query) {
+    // filter projects
+    let filteredProjects = projects.filter((project) => {
+      let values = Object.values(project).join('\n').toLowerCase();
+      return values.includes(query.toLowerCase());
+    });
+    return filteredProjects;
+};
+
+// Refactor all plotting into one function
+function renderPieChart(projectsGiven) {
+    // re-calculate rolled data
+    let newRolledData = d3.rollups(
+      projectsGiven,
+      (v) => v.length,
+      (d) => d.year,
+    );
+    // re-calculate data
+    let newData = newRolledData.map(([year, count]) => {
+      return { value: count, label: year }; // TODO
+    });
+    // re-calculate slice generator, arc data, arc, etc.
+    let newSliceGenerator = d3.pie().value((d) => d.value);
+    let newArcData = newSliceGenerator(newData);
+    let newArcs = newArcData.map((d) => arcGenerator(d));
+    // TODO: clear up paths and legends
+    let newSVG = d3.select('svg');
+    newSVG.selectAll('path').remove();
+    let newLegend = d3.select('.legend');
+    newLegend.selectAll('li').remove();
+    // update paths and legends, refer to steps 1.4 and 2.2
+    newArcs.forEach((arc, idx) => {
+        d3.select('svg')
+          .append('path')
+          .attr('d', arc)
+          .attr('fill', colors(idx)) 
+    })
+    newData.forEach((d, idx) => {
+        legend
+          .append('li')
+          .attr('style', `--color:${colors(idx)}`) 
+          .attr('class', 'legend-items')
+          .html(`<span class="swatch"></span> ${d.label} <em class="legend-value">(${d.value})</em>`); 
+    });
+}
+  
+// Call this function on page load
+renderPieChart(projects);
+
+searchInput.addEventListener('input', (event) => {
+  let filteredProjects = setQuery(event.target.value);
+  // re-render legends and pie chart when event triggers
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+  renderPieChart(filteredProjects);
+});
